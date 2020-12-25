16ca3b0124482bbf0cf71c217de9d4f6b8a36c19
lukas <anson.lukas.19@gmail.com>
2020-12-24 23:38:05 -0600

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Works</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}></Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
     </div>
   );
 }
 
 export default App;
+
+
+// TIME https://youtu.be/-cMqr9HpZ-Y?t=3309
