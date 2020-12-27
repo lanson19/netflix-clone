76357d57f4e24d69726a35dc57dfaddd233f27f5
lukas <anson.lukas.19@gmail.com>
2020-12-27 00:55:08 -0600

    Final Commit

--- src/Row.js
+++ src/Row.js
@@ -1,43 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
-import "./Row.css"
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+import "./Row.css";
 
-const base_url = "https://image.tmdb.org/t/p/original/"
+const base_url = "https://image.tmdb.org/t/p/original"
 
-function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-    // everytime the row loads all the info is loaded into the row
-    useEffect(() => {
-        // if left blank, run once when the row loads, and don't run it again
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+  useEffect(() => {
 
-    console.log(movies);
+    async function fetchData() {
 
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            
-            <div className="row_posters">
-                {/* {row poster} */}
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-                {movies.map(movie => (
-                    <img
-                        key = {movie.id}
-                        className="row_poster"
-                        src={`${base_url}${movie.poster_path}`} alt={movie.name}>
-                    </img>
-                ))}
-            </div>
-            {/* container -> posters */}
-        </div>
-    )
+  const opts = {
+    height: "390",
+    width: "99%",
+    playerVars: {
+      autoplay: 1,
+    }
+  }
+
+  const handleClick = (movie) => {
+    // console.table(movie?.title)
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.title || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        }).catch((error) => console.log(error));
+    }
+  }
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map(movie => {
+          return <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name} />
+        })}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
